825ef6dec9e5efc8b90582d19b3962a7493ffe0b
MailynIrinco <irincomailyn@gmail.com>
2022-06-04 03:38:19 +0800

    Starting point

---
+++ 10/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace _10
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.butt1 = new System.Windows.Forms.Label();
+            this.butt2 = new System.Windows.Forms.Label();
+            this.butt3 = new System.Windows.Forms.Label();
+            this.butt4 = new System.Windows.Forms.Label();
+            this.butt5 = new System.Windows.Forms.Label();
+            this.butt6 = new System.Windows.Forms.Label();
+            this.butt7 = new System.Windows.Forms.Label();
+            this.butt8 = new System.Windows.Forms.Label();
+            this.butt9 = new System.Windows.Forms.Label();
+            this.Restart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // butt1
+            // 
+            this.butt1.AccessibleName = "Buttons";
+            this.butt1.BackColor = System.Drawing.Color.FloralWhite;
+            this.butt1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.butt1.Font = new System.Drawing.Font("Segoe UI", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.butt1.Location = new System.Drawing.Point(31, 26);
+            this.butt1.Name = "butt1";
+            this.butt1.Size = new System.Drawing.Size(90, 86);
+            this.butt1.TabIndex = 0;
+            this.butt1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.butt1.Click += new System.EventHandler(this.butt1_Click);
+            // 
+            // butt2
+            // 
+            this.butt2.AccessibleName = "Buttons";
+            this.butt2.BackColor = System.Drawing.Color.FloralWhite;
+            this.butt2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.butt2.Font = new System.Drawing.Font("Segoe UI", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.butt2.Location = new System.Drawing.Point(127, 26);
+            this.butt2.Name = "butt2";
+            this.butt2.Size = new System.Drawing.Size(90, 86);
+            this.butt2.TabIndex = 1;
+            this.butt2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.butt2.Click += new System.EventHandler(this.butt2_Click);
+            // 
+            // butt3
+            // 
+            this.butt3.AccessibleName = "Buttons";
+            this.butt3.BackColor = System.Drawing.Color.FloralWhite;
+            this.butt3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.butt3.Font = new System.Drawing.Font("Segoe UI", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.butt3.Location = new System.Drawing.Point(223, 26);
+            this.butt3.Name = "butt3";
+            this.butt3.Size = new System.Drawing.Size(90, 86);
+            this.butt3.TabIndex = 2;
+            this.butt3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.butt3.Click += new System.EventHandler(this.butt3_Click);
+            // 
+            // butt4
+            // 
+            this.butt4.AccessibleName = "Buttons";
+            this.butt4.BackColor = System.Drawing.Color.FloralWhite;
+            this.butt4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.butt4.Font = new System.Drawing.Font("Segoe UI", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.butt4.Location = new System.Drawing.Point(31, 122);
+            this.butt4.Name = "butt4";
+            this.butt4.Size = new System.Drawing.Size(90, 86);
+            this.butt4.TabIndex = 3;
+            this.butt4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.butt4.Click += new System.EventHandler(this.butt4_Click);
+            // 
+            // butt5
+            // 
+            this.butt5.AccessibleName = "Buttons";
+            this.butt5.BackColor = System.Drawing.Color.FloralWhite;
+            this.butt5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.butt5.Font = new System.Drawing.Font("Segoe UI", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.butt5.Location = new System.Drawing.Point(127, 122);
+            this.butt5.Name = "butt5";
+            this.butt5.Size = new System.Drawing.Size(90, 86);
+            this.butt5.TabIndex = 4;
+            this.butt5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.butt5.Click += new System.EventHandler(this.butt5_Click);
+            // 
+            // butt6
+            // 
+            this.butt6.AccessibleName = "Buttons";
+            this.butt6.BackColor = System.Drawing.Color.FloralWhite;
+            this.butt6.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.butt6.Font = new System.Drawing.Font("Segoe UI", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.butt6.Location = new System.Drawing.Point(223, 122);
+            this.butt6.Name = "butt6";
+            this.butt6.Size = new System.Drawing.Size(90, 86);
+            this.butt6.TabIndex = 5;
+            this.butt6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.butt6.Click += new System.EventHandler(this.butt6_Click);
+            // 
+            // butt7
+            // 
+            this.butt7.AccessibleName = "Buttons";
+            this.butt7.BackColor = System.Drawing.Color.FloralWhite;
+            this.butt7.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.butt7.Font = new System.Drawing.Font("Segoe UI", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.butt7.Location = new System.Drawing.Point(31, 219);
+            this.butt7.Name = "butt7";
+            this.butt7.Size = new System.Drawing.Size(90, 86);
+            this.butt7.TabIndex = 6;
+            this.butt7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.butt7.Click += new System.EventHandler(this.butt7_Click);
+            // 
+            // butt8
+            // 
+            this.butt8.AccessibleName = "Buttons";
+            this.butt8.BackColor = System.Drawing.Color.FloralWhite;
+            this.butt8.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.butt8.Font = new System.Drawing.Font("Segoe UI", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.butt8.Location = new System.Drawing.Point(127, 219);
+            this.butt8.Name = "butt8";
+            this.butt8.Size = new System.Drawing.Size(90, 86);
+            this.butt8.TabIndex = 7;
+            this.butt8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.butt8.Click += new System.EventHandler(this.butt8_Click);
+            // 
+            // butt9
+            // 
+            this.butt9.AccessibleName = "Buttons";
+            this.butt9.BackColor = System.Drawing.Color.FloralWhite;
+            this.butt9.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.butt9.Font = new System.Drawing.Font("Segoe UI", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.butt9.Location = new System.Drawing.Point(223, 219);
+            this.butt9.Name = "butt9";
+            this.butt9.Size = new System.Drawing.Size(90, 86);
+            this.butt9.TabIndex = 8;
+            this.butt9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.butt9.Click += new System.EventHandler(this.butt9_Click);
+            // 
+            // Restart
+            // 
+            this.Restart.BackColor = System.Drawing.Color.LemonChiffon;
+            this.Restart.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Restart.BackgroundImage")));
+            this.Restart.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Restart.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Restart.ForeColor = System.Drawing.Color.Yellow;
+            this.Restart.Location = new System.Drawing.Point(123, 326);
+            this.Restart.Name = "Restart";
+            this.Restart.Size = new System.Drawing.Size(94, 44);
+            this.Restart.TabIndex = 9;
+            this.Restart.Tag = "";
+            this.Restart.UseVisualStyleBackColor = false;
+            this.Restart.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LemonChiffon;
+            this.ClientSize = new System.Drawing.Size(347, 385);
+            this.Controls.Add(this.Restart);
+            this.Controls.Add(this.butt9);
+            this.Controls.Add(this.butt8);
+            this.Controls.Add(this.butt7);
+            this.Controls.Add(this.butt6);
+            this.Controls.Add(this.butt5);
+            this.Controls.Add(this.butt4);
+            this.Controls.Add(this.butt3);
+            this.Controls.Add(this.butt2);
+            this.Controls.Add(this.butt1);
+            this.Name = "Form1";
+            this.Text = "I Tic Tac Toe Mo!";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Label butt1;
+        private Label butt2;
+        private Label butt3;
+        private Label butt4;
+        private Label butt5;
+        private Label butt6;
+        private Label butt7;
+        private Label butt8;
+        private Label butt9;
+        private Button Restart;
+    }
+}
---
+++ 10/Form1.cs
@@ -0,0 +1,210 @@
+namespace _10
+{
+    public partial class Form1 : Form
+    {
+        //TURN
+        bool turn = true;
+        byte turnCount = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //BUTTONS1-9
+        private void butt1_Click(object sender, EventArgs e)
+        {
+
+            if (turn == true)
+            {
+                butt1.Text = "X";
+            }
+            else butt1.Text = "O";
+            {
+
+                turn = !turn;
+                turnCount++;
+                butt1.Enabled = false;
+            }
+        }
+        private void butt2_Click(object sender, EventArgs e)
+        {
+            if (turn == true)
+            {
+                butt2.Text = "X";
+            }
+            else butt2.Text = "O";
+            {
+                turn = !turn;
+                turnCount++;
+                butt2.Enabled = false;
+            }
+        }
+
+        private void butt3_Click(object sender, EventArgs e)
+        {
+            if (turn == true)
+            {
+                butt3.Text = "X";
+            }
+            else butt3.Text = "O";
+            {
+                turn = !turn;
+                turnCount++;
+                butt3.Enabled = false;
+            }
+        }
+
+        private void butt4_Click(object sender, EventArgs e)
+        {
+            if (turn == true)
+            {
+                butt4.Text = "X";
+            }
+            else butt4.Text = "O";
+            {
+                turn = !turn;
+                turnCount++;
+                butt4.Enabled = false;
+            }
+        }
+
+        private void butt5_Click(object sender, EventArgs e)
+        {
+            if (turn == true)
+            {
+                butt5.Text = "X";
+            }
+            else butt5.Text = "O";
+            {
+                turn = !turn;
+                turnCount++;
+                butt5.Enabled = false;
+            }
+        }
+
+        private void butt6_Click(object sender, EventArgs e)
+        { 
+            if (turn == true)
+            {
+                butt6.Text = "X";
+            }
+            else butt6.Text = "O";
+            {
+                turn = !turn;
+                turnCount++;
+                butt6.Enabled = false;
+            }
+        }
+
+        private void butt7_Click(object sender, EventArgs e)
+        {
+            if (turn == true)
+            {
+                butt7.Text = "X";
+            }
+            else butt7.Text = "O";
+            {
+                turn = !turn;
+                turnCount++;
+                butt7.Enabled = false;
+            }
+        }
+
+        private void butt8_Click(object sender, EventArgs e)
+        {
+            if (turn == true)
+            {
+                butt8.Text = "X";
+            }
+            else butt8.Text = "O";
+            {
+                turn = !turn;
+                turnCount++;
+                butt8.Enabled = false;
+            }
+        }
+
+        private void butt9_Click(object sender, EventArgs e)
+        {
+            if (turn == true)
+            {
+                butt9.Text = "X";
+            }
+            else butt9.Text = "O";
+            {
+                turn = !turn;
+                turnCount++;
+                butt9.Enabled = false;
+            }
+        }
+
+        //THEWHOLE
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Button a = new Button();
+            a.Text = "";
+            a.Enabled = true;
+            turnCount = 0;
+            turn = true;
+        }
+
+        //RESTART
+        private void button1_Click(object sender, EventArgs e)
+        {
+            foreach (Control a in this.Controls)
+            {
+                Button b = new Button();
+                Restart = b;
+
+                a.Enabled = true;
+                a.Text = "";
+            }
+            turn = true;
+            turnCount = 0;
+        }
+
+        //CHECKFORWINNER
+        private void check_Winner () 
+        {
+            bool foraWinner = false;
+
+            //DIAGONAL
+            if ((butt1.Text == butt5.Text) && (butt5.Text == butt9.Text) && (butt1.Text != ""))
+                foraWinner = true;
+            else if ((butt3.Text == butt5.Text) && (butt5.Text == butt7.Text) && (butt5.Text != ""))
+                foraWinner = true;
+
+            //VERTICAL
+            else if ((butt1.Text == butt4.Text) && (butt4.Text == butt7.Text) && (butt4.Text != ""))
+                foraWinner = true;
+            else if ((butt2.Text == butt5.Text) && (butt5.Text == butt8.Text) && (butt5.Text != ""))
+                foraWinner = true;
+            else if ((butt3.Text == butt6.Text) && (butt6.Text == butt9.Text) && (butt6.Text != ""))
+                foraWinner = true;
+
+            //HORIZONTAL
+            else if ((butt1.Text == butt2.Text) && (butt2.Text == butt3.Text) && (butt1.Text != ""))
+                foraWinner = true;
+            else if ((butt4.Text == butt5.Text) && (butt5.Text == butt6.Text) && (butt4.Text != ""))
+                foraWinner = true;
+            else if ((butt7.Text == butt8.Text) && (butt8.Text == butt9.Text) && (butt7.Text != ""))
+                foraWinner = true;
+
+            if (foraWinner)
+            {
+                string winner = "";
+                if (turn)
+                    winner = "O";
+                else
+                    winner = "X";
+
+                MessageBox.Show("NANALO KA NG WASHING MACHINE!");
+            }
+            else if (turnCount == 9) ;
+            {
+                MessageBox.Show("It's a Draw" + "Click restart for a new game");
+            }
+        }
+    }
+}
